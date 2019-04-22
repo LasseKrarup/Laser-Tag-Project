40ad3424c4767da18fbb5ddf56b90b972cf53af2
Fresi <36982705+frederiksidenius@users.noreply.github.com>
2019-04-22 20:04:24 +0200

    Sometimes wrong detection

--- PSoC/practiceKitCtrlApp.cydsn/PracticeKitComUnitIF.h
+++ PSoC/practiceKitCtrlApp.cydsn/PracticeKitComUnitIF.h
@@ -8,9 +8,30 @@
 #ifndef PRACTICE_KIT_COM_UNIT_IF_H
 #define PRACTICE_KIT_COM_UNIT_IF_H
 
-void receiverHit(int currentLaserId)
+#include "project.h"
+    
+void receiverHit(int currentLaserID)
 {
-    currentLaserId = 0;
+    switch(currentLaserID)
+    {
+        case 0:
+            Pin_0_Write(~Pin_0_Read());
+            break;
+        case 1:
+            Pin_1_Write(~Pin_1_Read());
+            break;
+        case 2:
+            Pin_2_Write(~Pin_2_Read());
+            break;
+        case 3:
+            Pin_3_Write(~Pin_3_Read());
+            break;
+        case 4:
+            Pin_4_Write(~Pin_4_Read());
+            break;
+        default:
+            break;
+    }
 }
 
 #endif /* PRACTICE_KIT_COM_UNIT_IF_H */ 
--- PSoC/practiceKitCtrlApp.cydsn/main.c
+++ PSoC/practiceKitCtrlApp.cydsn/main.c
@@ -14,9 +14,9 @@ static const float minLevelDetection = 1.0;   // Minimum level for detection
 float filterOutputVolt = 0;                 // Holds filter output in voltage
 uint16 currentLaserID = 0;                  // Holds current laser id
 
-int test = 0;
-int test2 = 0;
-int test3 = 0;
+// int test = 0;
+// int test2 = 0;
+// int test3 = 0;
 
 CY_ISR_PROTO(isr_filter_handler);
 CY_ISR_PROTO(isr_mixerFreq_handler);
@@ -30,6 +30,7 @@ int main(void)
     isr_filter_StartEx(isr_filter_handler);         // Start filter isr
     isr_mixerFreq_StartEx(isr_mixerFreq_handler);   // Start mixerFreq isr
     
+    
     for(;;)
     {
         // Empty loop
@@ -38,25 +39,30 @@ int main(void)
 
 CY_ISR(isr_filter_handler)
 {
+    // isr_mixerFreq_Disable();
+    
     filterOutputVolt = ADC_DelSig_CountsTo_Volts(filterOutput);
-    test++;
+    // test++;
     
     if ((filterOutputVolt > minLevelDetection) | (filterOutputVolt < -minLevelDetection))
     {
-        test2++;
-        // Disable interrupt
-        // receiverHit(currentLaserID)
+        // test2++;
+        // CyGlobalIntDisable; // Disable global iterrupts
+        receiverHit(currentLaserID);
         // Evt. sleep???
         // Evt. clear pending interrupts
-        // Enable interrupts         
+        // CyGlobalIntEnable;  // Enable global interrupts
     }
+    
+    // isr_filter_ClearPending();
+    // isr_mixerFreq_Enable();
 }
 
 CY_ISR(isr_mixerFreq_handler)
 { 
-    test3++;
+    // test3++;
+
     currentLaserID = changeMixerFrequency(currentLaserID);
-    // Reset ADC og filter?
 }
 
 /* [] END OF FILE */
