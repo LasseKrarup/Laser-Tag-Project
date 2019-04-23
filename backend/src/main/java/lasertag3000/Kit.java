27a9d9cadd3178c98168d91f5eb77caae6bab4de
Leonard Matthias Günther Hansen <leonard@stormcraft.de>
2019-04-24 01:23:04 +0200

    .

---
+++ backend/src/main/java/lasertag3000/Kit.java
@@ -0,0 +1,5 @@
+package main.java.lasertag3000;
+
+public class Kit {
+    
+}
---
+++ backend/src/main/java/lasertag3000/Player.java
@@ -0,0 +1,13 @@
+package main.java.lasertag3000;
+
+public class Player {
+    private int id;
+    private string name;
+     Kit _kit;
+
+    public Player(int _id, String _name, Kit _kit2){
+        
+    }
+
+
+}
