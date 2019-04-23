77e9893a81ee8fa68fb19167211d142303bdf71c
Leonard Matthias Günther Hansen <leonard@stormcraft.de>
2019-04-24 00:17:31 +0200

    First

---
+++ backend/src/main/java/lasertag3000/App.java
@@ -0,0 +1,17 @@
+package lasertag3000;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ backend/src/main/java/lasertag3000/SQLAPI.java
@@ -0,0 +1,52 @@
+package main.java.lasertag3000;
+
+import com.sun.tools.javac.util.List;
+
+import Kit;
+
+public class SQLAPI {
+
+    private string server;
+
+    public SQLAPI(){
+
+    }
+    //Kit functions
+    public boolean addKit() {
+
+    }
+
+    public boolean removeKit() {
+
+    }
+
+    public Kit[] getKits(){
+
+    }
+
+    //Game functions
+    public boolean addGame(){
+
+    }
+
+    public boolean addPlayer(){
+
+    }
+
+    public boolean removePlayer(){
+
+    }
+
+    public boolean PlayerShot(){
+        
+    }
+
+    public boolean clearHighScore(){
+
+    }
+
+    public boolean addPractice(){
+
+    }
+
+}
