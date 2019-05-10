4e7d10c9598e60598aafedf9727ce0256e63dfcd
Lasse Krarup <au579525@uni.au.dk>
2019-05-10 08:23:16 +0200

    Updated driver header

--- i2c_driver/src/i2c_driver.hpp
+++ i2c_driver/src/i2c_driver.hpp
@@ -3,22 +3,36 @@
 
 #include <MsgQueue.hpp>
 #include <pthread.h>
+#include <queue>
 
 class i2cDriver {
 public:
   i2cDriver();
   void send(char &buf);
   char receive();
-  unsigned char slaveRequest = 0;
-  unsigned char dataReadyFlag = 0;
+  unsigned char getDataReadyFlag();
 
 private:
+  std::queue<char> dataQueue;
   void i2cReaderEventHandler();
   void i2cSenderEventHandler();
   pthread_t i2cReaderThread;
   pthread_t i2cSenderThread;
   MsgQueue i2cReaderMsgQ;
   MsgQueue i2cSenderMsgQ;
+  void i2cSendByte(char byte);
+  void i2cReceiveByte();
+  unsigned char slaveRequest = 0;
+  unsigned char dataReadyFlag = 0;
 };
+
+struct i2cMessage : public Message {
+  char data;
+};
+
+enum {
+  I2C_SLAVE_REQ, // A slave requests the master to read from it
+  I2C_SEND
+}; // Msg ID's
 
 #endif /*I2C_DRIVER_HPP*/
