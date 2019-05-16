7923191624ac3cce7c92ee651771ededb4912af4
Lasse Krarup <au579525@ase.au.dk>
2019-05-17 01:03:05 +0200

    Test server for practice kit functionality

--- WebSocket server test/index.js
+++ WebSocket server test/index.js
@@ -8,6 +8,9 @@ let mockScore = Math.floor(Math.random() * 100);
 
 wss.on("connection", function connection(ws) {
     console.log("Client connected");
+
+    ws.send(JSON.stringify({ action: "startPractice" }));
+
     ws.on("message", function incoming(message) {
         console.log("received:");
         let parsedMsg = JSON.parse(message);
@@ -16,16 +19,16 @@ wss.on("connection", function connection(ws) {
         // ws.send(message); // echo back to client
 
         // Send a high score update (for testing)
-        if (parsedMsg.action == "addPlayer") {
-            ws.send(
-                JSON.stringify({
-                    action: "highscoreUpdate",
-                    id: parsedMsg.id,
-                    score: mockScore
-                })
-            );
-        }
-        mockScore = Math.floor(Math.random() * 100);
+        // if (parsedMsg.action == "addPlayer") {
+        //     ws.send(
+        //         JSON.stringify({
+        //             action: "highscoreUpdate",
+        //             id: parsedMsg.id,
+        //             score: mockScore
+        //         })
+        //     );
+        // }
+        // mockScore = Math.floor(Math.random() * 100);
     });
 
     ws.on("close", function close(ws) {
